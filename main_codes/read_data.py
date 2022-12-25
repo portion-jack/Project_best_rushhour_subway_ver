d8019de1f5142a71f90f7e2c8cad51c89037ad4e
portion-jack <flycast0080@gmail.com>
2022-12-25 11:53:38 +0900

    read_data and show graphs

---
+++ main_codes/read_data.py
@@ -0,0 +1,31 @@
+import glob
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.rcParams["date.autoformatter.minute"] = "%H:%M"
+plt.rcParams['font.family']='AppleGothic'
+
+
+def read_congest_section():
+    congest_path=glob.glob('data/final_data/*congest_section.csv')
+
+    tmp_li = list()
+    for congest in congest_path:
+        tmp_li.append(pd.read_csv(congest,index_col=0))
+    df_congest = pd.concat(tmp_li)
+
+    df_congest['time']=df_congest['hh'].apply(lambda x: str(x).zfill(2)) + df_congest['mm'].apply(lambda x: str(x).zfill(2))
+    df_congest['time']=pd.to_datetime(df_congest['time'],format='%H%M')
+    return df_congest
+
+def read_congest():
+    congest_path=glob.glob('data/final_data/*congest.csv')
+
+    tmp_li = list()
+    for congest in congest_path:
+        tmp_li.append(pd.read_csv(congest,index_col=0))
+    df_congest = pd.concat(tmp_li)
+
+    df_congest['time']=df_congest['hh'].apply(lambda x: str(x).zfill(2)) + df_congest['mm'].apply(lambda x: str(x).zfill(2))
+    df_congest['time']=pd.to_datetime(df_congest['time'],format='%H%M')
+    return df_congest
